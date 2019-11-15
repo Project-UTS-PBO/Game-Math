5b6a3eabe160f6a775fde6b15e1ffa01af2707f0
Daniel <57202243+DanielErick-010@users.noreply.github.com>
2019-11-15 20:22:57 +0700

    Add files via upload

---
+++ src/Entitas/MainClass.java
@@ -0,0 +1,25 @@
+package Entitas;
+
+
+public class MainClass {
+
+    // Main class.
+    public static void main(String[] args) {
+
+        // Buat objek baru dan instansiasi untuk nilai awal.
+        Login player = new Login();
+        
+        // Masuk ke tampilan utama untuk input nama.
+        tampilan.tampilanUtama(player);
+
+        // Masuk ke menu Utama.
+        // Masuk ke tampilan Menu.
+        tampilan.tampilanMenu(player);
+       
+
+
+   }
+
+}
+
+    
